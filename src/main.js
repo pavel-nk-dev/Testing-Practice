f2a9ba3bfcba39d387da3adffc83628dc13a03e5
pav_nk <pav_nk@vk.com>
2025-03-18 08:53:11 +0300

    reverse string and calculator tests added

--- src/main.js
+++ src/main.js
@@ -6,4 +6,32 @@ function capitalize(str = "") {
   return normalizedStr.charAt(0).toUpperCase() + normalizedStr.slice(1);
 }
 
-export { capitalize };
+function reverseString(str = "") {
+  if (typeof str !== "string") return "";
+  return str.trim().split("").reverse().join("");
+}
+
+const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+
+  divide(num1, num2) {
+    if (num2 === 0) return false;
+    return num1 / num2;
+  },
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
+};
+
+function caesarCipher(str, shift) {}
+
+function analyzeArray(array) {}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- src/test.js
+++ src/test.js
@@ -1,4 +1,10 @@
-import { capitalize } from "./main";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./main";
 
 test("capitalize", () => {
   expect(capitalize("let's go!")).toBe("Let's go!");
@@ -8,3 +14,31 @@ test("capitalize", () => {
   expect(capitalize(100)).toBe("");
   expect(capitalize(undefined)).toBe("");
 });
+
+test("reverse string", () => {
+  expect(reverseString("let's go!")).toBe("!og s'tel");
+  expect(reverseString("javascript")).toBe("tpircsavaj");
+  expect(reverseString("   react   ")).toBe("tcaer");
+  expect(reverseString("123")).toBe("321");
+  expect(reverseString(100)).toBe("");
+  expect(reverseString(null)).toBe("");
+});
+
+test("calculator", () => {
+  expect(calculator.add(2, 3)).toBe(5);
+  expect(calculator.add(-1, 1)).toBe(0);
+  expect(calculator.add(0, 0)).toBe(0);
+  expect(calculator.subtract(5, 3)).toBe(2);
+  expect(calculator.subtract(1, 1)).toBe(0);
+  expect(calculator.subtract(0, 0)).toBe(0);
+  expect(calculator.divide(6, 3)).toBe(2);
+  expect(calculator.divide(1, 1)).toBe(1);
+  expect(calculator.divide(0, 0)).toBe(false);
+  expect(calculator.multiply(3, 3)).toBe(9);
+  expect(calculator.multiply(1, 2)).toBe(2);
+  expect(calculator.multiply(1, 1)).toBe(1);
+});
+
+test("caesar cipher", () => {});
+
+test("analyze array", () => {});
