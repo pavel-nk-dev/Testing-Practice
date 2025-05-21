e908ed4db71303d3bcb6514b01496c755f1c6296
pavel <pav-n-k@yandex.ru>
2025-05-21 08:14:24 +0300

    task CaesarCipher is added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-function capitalize(str = "") {
+const capitalize = (str = "") => {
   if (typeof str !== "string") return "";
   const normalizedStr = str.trim();
   return normalizedStr.charAt(0).toUpperCase() + normalizedStr.slice(1);
-}
+};
 
-function reverseString(str = "") {
+const reverseString = (str = "") => {
   if (typeof str !== "string") return "";
   return str.trim().split("").reverse().join("");
-}
+};
 
 const calculator = {
   add(num1, num2) {
@@ -29,8 +29,62 @@ const calculator = {
     return num1 * num2;
   },
 };
+const caesarCipher = (str, shift) => {
+  const ENG_ALPHABET = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
-function caesarCipher(str, shift) {}
+  const isLetter = (char) => {
+    const code = char.charCodeAt(0);
+    return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+  };
+
+  const isLowerCaseLetter = (letter) => {
+    console.log(letter);
+    return letter.toLowerCase() === letter;
+  };
+
+  const result = str
+    .split("")
+    .map((letter) => {
+      console.log(letter);
+      if (!isLetter(letter)) return letter;
+      const cipher =
+        ENG_ALPHABET[(ENG_ALPHABET.indexOf(letter.toLowerCase()) + shift) % 26];
+
+      console.log(letter, cipher);
+      if (isLowerCaseLetter(letter)) return cipher;
+      return cipher.toUpperCase();
+    })
+    .join("");
+
+  return result;
+};
 
 function analyzeArray(array) {}
 
